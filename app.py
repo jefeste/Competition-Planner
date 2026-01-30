923ab488e1a803ac8bb0933b163e6c179a059c5a
jefeste <digard.jeremy@yahoo.com>
2026-01-30 10:47:33 +0100

    Update link to Jérémy Digard's website

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ st.markdown(
             .sidebar-spacer { flex: 1 1 auto; }
         </style>
         <div class="made-by">
-            Made by <a href="www.jeremydigard.com" target="_blank">Jérémy Digard</a> for
+            Made by <a href="http://www.jeremydigard.com" target="_blank">Jérémy Digard</a> for
             <a href="https://www.equissima.ch" target="_blank">Equissima</a>
             2025- 2026
         </div>
